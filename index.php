231795b6bbd9bd2cefadd3569c1ead96763f61f0
C836907548 <157164780+C836907548@users.noreply.github.com>
2024-04-02 13:06:18 -0700

    Rough draft for the homepage. The nav bar, ...
    carousel, and links are temporary place holders. Will finish them after I do my part for the actual color coordinator. I'm happy to make any changes to the home page. Its aesthetic is defiantly a work in progress.

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Source: https://www.w3schools.com/tags/tag_meta.asp  -->
+    <meta charset="UTF-8">
+    <meta name="author" content="Kyle Jager, David Portilla, Zach Stull">
+    <meta name="keywords" content="Homepage, CSU, Web, Development">
+    <meta name="description" content="Team37's Homepage">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="styleIndex.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+    <!-- <header>Homepage</header> -->
+
+
+    <!-- Place holder navbar -->
+    <div class="navHome">
+        <img src="https://techspective.net/wp-content/uploads/2016/08/Apple-logo.jpg" alt="Logo" class="navLogo">
+        <a href="https://www.cs.colostate.edu:4444/~c836907548/Team37/">Home</a>
+        <a href="https://www.cs.colostate.edu:4444/~c836907548/Team37/colors">Colors</a>
+        <a href="https://www.cs.colostate.edu:4444/~c836907548/Team37/about">About</a>
+    </div>
+
+    <div class="buffer"></div>
+
+    <div class="signHome">
+        <h1>Color Picker</h1>
+        <h2>By Teamname</h2>
+    </div>
+
+    <div class="paintContainerHome">
+        <div class="paintHome"></div>
+        <img src="images/brushbigflipped.png" class="brushHome">
+    </div>
+
+    <div class="buffer"></div>
+
+    <!-- /////////////// carousel /////////////////// -->
+    <!-- place holder -->
+    <!-- https://codepen.io/pistell/pen/BqGpvR -->
+    <!-- I will replace this with a custom carousel after I finish my part of the color coordinate generator -->
+
+    <div class="col-sm-7 mx-auto">
+        <header class="section-header text-center">
+            <span class="h1 d-block">
+            </span>
+            <span class="customerstories" h2>Customer Stories</h2>
+        </header>
+    </div>
+
+    <div id="flex-container" class="testimonials">
+        <div id="left-zone">
+            <ul class="list">
+                <li class="item">
+                    <input type="radio" id="radio_testimonial-1" name="basic_carousel" checked="checked" />
+                    <label class="label_testimonial-1" for="radio_testimonial-1">Diamond Pest Elimination</label>
+                    <div class="content-test content_testimonial-1">
+                        <span class="picto"></span>
+                        <h1>Diamond Pest Elimination</h1>
+                        <p>“The team really takes pride in their work. If I didn’t know any better I would think they
+                            actually worked for my company.”</p>
+                        <p class="testimonialFrom">Bill, Owner</p>
+                        <p class="testimonialState">Rochester, NY</p>
+                    </div>
+                </li>
+                <li class="item">
+                    <input type="radio" id="radio_testimonial-2" name="basic_carousel" />
+                    <label class="label_testimonial-2" for="radio_testimonial-2">A+ Handyman Service</label>
+                    <div class="content-test content_testimonial-2">
+                        <span class="picto"></span>
+                        <h1>A+ Handyman Service</h1>
+                        <p>“Quite simply… the service offers prompt response time to my visitors and helps me to better
+                            know what type of project a potential customer wants.”</p>
+                        <p class="testimonialFrom">Bill, Owner</p>
+                        <p class="testimonialState">Tucson, AZ</p>
+                        <br>
+                    </div>
+                </li>
+                <li class="item">
+                    <input type="radio" id="radio_testimonial-3" name="basic_carousel" />
+                    <label class="label_testimonial-3" for="radio_testimonial-3">Mod Movers</label>
+                    <div class="content-test content_testimonial-3">
+                        <span class="picto"></span>
+                        <h1>Mod Movers</h1>
+                        <p>“I couldn’t believe it. I actually had to hire someone to help me keep up with the new
+                            business. I had no idea my website had so much value.”</p>
+                        <p class="testimonialFrom">Marlene, Owner</p>
+                        <p class="testimonialState">Monterey, CA</p>
+                    </div>
+                </li>
+                <li class="item">
+                    <input type="radio" id="radio_testimonial-4" name="basic_carousel" />
+                    <label class="label_testimonial-4" for="radio_testimonial-4">AK Pest Control</label>
+                    <div class="content-test content_testimonial-4">
+                        <span class="picto"></span>
+                        <h1>AK Pest Control</h1>
+                        <p>Great company to send leads. Very efficient and pleased with the services. We get lots of
+                            leads and that whats important. Support is also great from the managers/support. Thanks YPC
+                            Chat</p>
+                        <p class="testimonialFrom">Mark, Owner</p>
+                        <p class="testimonialState">Somerset, VA</p>
+                        <br>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div id="right-zone"></div>
+    </div>
+
+    <!-- /////////////////////////////////// -->
+
+    <div class="buffer"></div>
+
+    <hr>
+
+
+    </footer>
+    <div class="linkcontainer">
+        <!-- place holder buttons and links -->
+        <a href="https://www.linkedin.com/" class="fa fa-linkedin"></a>
+        <a href="https://www.instagram.com/" class="fa fa-instagram"></a>
+        <a href="https://rss.com/" class="fa fa-rss"></a>
+    </div>
+
+
+</html>

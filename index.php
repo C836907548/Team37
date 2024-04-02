d00c14e867537a16ba706c7042643230baa4f49b
Zachary Stull <stullz@keystone.cs.colostate.edu>
2024-04-02 16:18:01 -0600

    Initial Homepage Layout

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <header>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <meta name="author" content="Zach Stull, Kyle Jager, Dave Portilla">
+        <title> Color Picker </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        </header>
+        <div id="navbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="color.php">Colors</a></li>
+                <li style="float:right"><a href="about.php">About</a></li>
+              </ul>
+        </div>
+        <main>
+            <div class="contents">
+                <img src="https://pixy.org/images/placeholder.png" alt="placeholder-image" align="right" height=35% width=35% style="margin-top: 100px;">
+                <h1>Placeholder Title</h1><br>
+                <hr>
+                <p style="font-size:20px; ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                et dolore magna aliqua. Erat nam at lectus urna duis convallis convallis tellus. Ipsum dolor sit amet consectetur. Tempor commodo 
+                ullamcorper a lacus vestibulum sed arcu. Felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Habitant morbi tristique 
+                senectus et netus et malesuada. Tellus elementum sagittis vitae et leo duis ut diam. Purus faucibus ornare suspendisse sed. Consequat nisl 
+                vel pretium lectus quam id leo in vitae. Pharetra sit amet aliquam id. Massa id neque aliquam vestibulum morbi blandit. Purus semper eget 
+                duis at tellus. Risus in hendrerit gravida rutrum quisque. Augue ut lectus arcu bibendum at varius vel. Risus nullam eget felis eget. Justo 
+                laoreet sit amet cursus sit amet. Non sodales neque sodales ut etiam sit amet nisl purus.</p> 
+                <p style="font-size:20px; ">Convallis convallis tellus id interdum velit. Risus commodo viverra maecenas accumsan lacus vel 
+                facilisis. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer. Aliquet sagittis id consectetur purus. Tempor 
+                commodo ullamcorper a lacus vestibulum sed arcu non odio. Interdum velit laoreet id donec ultrices tincidunt. Amet massa vitae tortor 
+                condimentum lacinia quis vel. Proin sed libero enim sed faucibus turpis in eu. Euismod lacinia at quis risus sed vulputate odio ut enim. 
+                Nisi quis eleifend quam adipiscing vitae proin sagittis. Amet dictum sit amet justo donec enim diam. Tortor at risus viverra adipiscing 
+                at in.</p>
+            </div>
+        </main>
+    </body>
+</html>

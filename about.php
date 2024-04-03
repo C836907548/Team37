f74d06e8494087a445791851944cadc1caad16ec
Zachary Stull <stullz@keystone.cs.colostate.edu>
2024-04-03 13:37:16 -0600

    About Page Initial

--- about.php
+++ about.php
@@ -1,50 +1,79 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>About Our Team</title>
-    <style>
-      .team-member {
-        display: flex;
-        align-items: center;
-        margin-bottom: 30px;
-      }
+<html lang="en">
 
-      .team-member img {
-        width: 150px;
-        height: 150px;
-        border-radius: 50%;
-        margin-right: 20px;
-      }
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="author" content="Zach Stull, Kyle Jager, Dave Portilla">
+    <title> Color Picker </title>
+    <meta name="keywords" content="Homepage, CSU, Web, Development">
+    <meta name="description" content="Team37's Homepage">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="styleIndex.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<!DOCTYPE html>
+<html lang="en">
 
-      .team-member h3 {
-        margin-top: 0;
-      }
+<body>
+    <div id="navbar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="colors.php">Colors</a></li>
+            <li style="float:right"><a href="about.php">About</a></li>
+        </ul>
+    </div>
 
-      .team-member p {
-        margin-bottom: 0;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>About Our Team</h1>
-    <div class="team-member">
-      <img src="team-member1.jpg" alt="Team Member 1">
-      <div>
-        <h3>name one</h3>
-        <p>bio one</p>
-      </div>
+    <div class="signHome">
+        <h1>About Us</h1>
     </div>
-    <div class="team-member">
-      <img src="team-member2.jpg" alt="Team Member 2">
-      <div>
-        <h3>name two</h3>
-        <p>bio two</p>
-      </div>
+
+    <div class="paintContainerHome">
+        <div class="paintHome"></div>
+        <img src="images/brushbigflipped.png" class="brushHome">
     </div>
-    <div class="team-member">
-      <img src="team-member3.jpg" alt="Team Member 3">
-      <div>
-        <h3>name three</h3>
-        <p>bio three</p>
-      </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/480px-Placeholder_no_text.png" alt="Jane" style="width:50%" style="height:50%">
+                <div class="container">
+                    <h2>Zach Stull</h2>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/480px-Placeholder_no_text.png" alt="Mike" style="width:50%" style="height:50%">
+                <div class="container">
+                    <h2>Kyle Jager</h2>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/480px-Placeholder_no_text.png" alt="John" style="width:50%" style="height:50%">
+                <div class="container">
+                    <h2>David Portilla</h2>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                </div>
+            </div>
+        </div>
     </div>
+
+    </footer>
+    <div class="linkcontainer">
+        <!-- place holder buttons and links -->
+        <a href="https://www.linkedin.com/" class="fa fa-linkedin"></a>
+        <a href="https://www.instagram.com/" class="fa fa-instagram"></a>
+        <a href="https://rss.com/" class="fa fa-rss"></a>
+    </div>
+
+
+</html>

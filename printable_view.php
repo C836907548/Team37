8da96672b9c56b052118ab303d4dfe0261397b44
C836907548 <157164780+C836907548@users.noreply.github.com>
2024-04-07 17:43:44 -0800

    Fixed print view. Colors2 js still needs work.
    Its very inefficient code but it works.

--- printable_view.php
+++ printable_view.php
@@ -22,7 +22,6 @@
     <br>
     <br>
 
-
     <div class="logo-print">
         <img src="images/logo-idea1.png" alt="logo placeholder">
     </div>
@@ -32,86 +31,121 @@
 
     <?php
 
-
-
-    $select0 = $_POST['select0'];
-    echo $select0;
-    $select1 = $_POST['select1'];
-    echo $select1;
-
-    // $min_rows_cols = 1;
-// $max_rows_cols = 26;
-// $min_colors = 1;
-// $max_colors = 10;
-    
-
     $rows_cols = isset($_POST["rows_cols"]) ? intval($_POST["rows_cols"]) : null;
     $colors = isset($_POST["colors"]) ? intval($_POST["colors"]) : null;
-    echo $rows_cols;
-    echo $colors;
 
-    $select0 = $_POST['select0'];
-    echo $select0;
-    $select1 = $_POST['select1'];
-    echo $select1;
-    $select2 = $_POST['select2'];
-    echo $select2;
-    $select3 = $_POST['select3'];
-    echo $select3;
-    $select4 = $_POST['select4'];
-    echo $select4;
-    $select5 = $_POST['select5'];
-    echo $select5;
-    $select6 = $_POST['select6'];
-    echo $select6;
-    $select7 = $_POST['select7'];
-    echo $select7;
-    $select8 = $_POST['select8'];
-    echo $select8;
-    $select9 = $_POST['select9'];
-    echo $select9;
-
-    // $color1 = isset($_GET["color_selected"]) ? intval($_GET["color_selected"]) : null;
-    // $color2 = isset($_GET["color_selected[]"]);
-    // $color3 = isset($_GET["rows_cols"]);
+    // this resultings in all data being "0". Im not sure why
+    //
+    // $select0 = isset($_POST["select0"]) ? intval($_POST["select0"]) : null;
+    // $select1 = isset($_POST["select1"]) ? intval($_POST["select1"]) : null;
+    // $select2 = isset($_POST["select2"]) ? intval($_POST["select2"]) : null;
+    // $select3 = isset($_POST["select3"]) ? intval($_POST["select3"]) : null;
+    // $select4 = isset($_POST["select4"]) ? intval($_POST["select4"]) : null;
+    // $select5 = isset($_POST["select5"]) ? intval($_POST["select5"]) : null;
+    // $select6 = isset($_POST["select6"]) ? intval($_POST["select6"]) : null;
+    // $select7 = isset($_POST["select7"]) ? intval($_POST["select7"]) : null;
+    // $select8 = isset($_POST["select8"]) ? intval($_POST["select8"]) : null;
+    // $select9 = isset($_POST["select9"]) ? intval($_POST["select9"]) : null;  
     
-    // $rows_cols = isset($_POST["rows_cols"]) ? intval($_POST["rows_cols"]) : null;
-// $colors = isset($_POST["colors"]) ? intval($_POST["colors"]) : null;
-    
+    for ($i = 0; $i < $colors; $i++) {
+        if ($i == 0) {
+            $select0 = $_POST["select0"];
+        }
+        if ($i == 1) {
+            $select0 = $_POST["select0"];
+            $select1 = $_POST["select1"];
+        }
 
-    $color1 = isset($_GET["resultColorValue"]) ? intval($_GET["resultColorValue"]) : null;
+        if ($i == 2) {
+            $select0 = $_POST["select0"];
+            $select1 = $_POST["select1"];
+            $select2 = $_POST["select2"];
+        }
 
-    ?>
-    <?php
+        if ($i == 3) {
+            $select0 = $_POST["select0"];
+            $select1 = $_POST["select1"];
+            $select2 = $_POST["select2"];
+            $select3 = $_POST["select3"];
+        }
 
+        if ($i == 4) {
+            $select0 = $_POST["select0"];
+            $select1 = $_POST["select1"];
+            $select2 = $_POST["select2"];
+            $select3 = $_POST["select3"];
+            $select4 = $_POST["select4"];
+        }
+
+        if ($i == 5) {
+            $select0 = $_POST["select0"];
+            $select1 = $_POST["select1"];
+            $select2 = $_POST["select2"];
+            $select3 = $_POST["select3"];
+            $select4 = $_POST["select4"];
+            $select5 = $_POST["select5"];
+        }
+
+        if ($i == 6) {
+            $select0 = $_POST["select0"];
+            $select1 = $_POST["select1"];
+            $select2 = $_POST["select2"];
+            $select3 = $_POST["select3"];
+            $select4 = $_POST["select4"];
+            $select5 = $_POST["select5"];
+            $select6 = $_POST["select6"];
+        }
+
+        if ($i == 7) {
+            $select0 = $_POST["select0"];
+            $select1 = $_POST["select1"];
+            $select2 = $_POST["select2"];
+            $select3 = $_POST["select3"];
+            $select4 = $_POST["select4"];
+            $select5 = $_POST["select5"];
+            $select6 = $_POST["select6"];
+            $select7 = $_POST["select7"];
+        }
+
+        if ($i == 8) {
+            $select0 = $_POST["select0"];
+            $select1 = $_POST["select1"];
+            $select2 = $_POST["select2"];
+            $select3 = $_POST["select3"];
+            $select4 = $_POST["select4"];
+            $select5 = $_POST["select5"];
+            $select6 = $_POST["select6"];
+            $select7 = $_POST["select7"];
+            $select8 = $_POST["select8"];
+        }
+
+        if ($i == 9) {
+            $select0 = $_POST["select0"];
+            $select1 = $_POST["select1"];
+            $select2 = $_POST["select2"];
+            $select3 = $_POST["select3"];
+            $select4 = $_POST["select4"];
+            $select5 = $_POST["select5"];
+            $select6 = $_POST["select6"];
+            $select7 = $_POST["select7"];
+            $select8 = $_POST["select8"];
+            $select9 = $_POST["select9"];
+        }
+    }
 
     echo "<table class='color-table'>";
     for ($i = 0; $i < $colors; $i++) {
         if ($i == 0) {
             echo "<tr>";
-            echo "<td>" . $color1 . "</td>";
+            echo "<td>" . $select0 . "</td>";
+            // echo "<td><p>" . $select0 . "<p></td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
         }
         if ($i == 1) {
             echo "<tr>";
-            echo "<td style='width: 20%;'>";
-            echo "<select name='color' id='select1'>";
-
-            echo "<option value='orange'>orange</option>";
-            echo "<option value='yellow'>yellow</option>";
-            echo "<option value='green'>green</option>";
-            echo "<option value='blue'>blue</option>";
-            echo "<option value='purple'>purple</option>";
-            echo "<option value='grey'>grey</option>";
-            echo "<option value='brown'>brown</option>";
-            echo "<option value='black'>black</option>";
-            echo "<option value='teal'>teal</option>";
-            echo "<option value='red'>red</option>";
-
-            echo "</select>";
-            echo "</td>";
+            echo "<td>" . $select1 . "</td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
@@ -119,22 +153,7 @@
 
         if ($i == 2) {
             echo "<tr>";
-            echo "<td style='width: 20%;'>";
-            echo "<select name='color' id='select2'>";
-
-            echo "<option value='yellow'>yellow</option>";
-            echo "<option value='green'>green</option>";
-            echo "<option value='blue'>blue</option>";
-            echo "<option value='purple'>purple</option>";
-            echo "<option value='grey'>grey</option>";
-            echo "<option value='brown'>brown</option>";
-            echo "<option value='black'>black</option>";
-            echo "<option value='teal'>teal</option>";
-            echo "<option value='red'>red</option>";
-            echo "<option value='orange'>orange</option>";
-
-            echo "</select>";
-            echo "</td>";
+            echo "<td>" . $select2 . "</td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
@@ -142,22 +161,7 @@
 
         if ($i == 3) {
             echo "<tr>";
-            echo "<td style='width: 20%;'>";
-            echo "<select name='color' id='select3'>";
-
-            echo "<option value='green'>green</option>";
-            echo "<option value='blue'>blue</option>";
-            echo "<option value='purple'>purple</option>";
-            echo "<option value='grey'>grey</option>";
-            echo "<option value='brown'>brown</option>";
-            echo "<option value='black'>black</option>";
-            echo "<option value='teal'>teal</option>";
-            echo "<option value='red'>red</option>";
-            echo "<option value='orange'>orange</option>";
-            echo "<option value='yellow'>yellow</option>";
-
-            echo "</select>";
-            echo "</td>";
+            echo "<td>" . $select3 . "</td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
@@ -165,22 +169,7 @@
 
         if ($i == 4) {
             echo "<tr>";
-            echo "<td style='width: 20%;'>";
-            echo "<select name='color' id='select4'>";
-
-            echo "<option value='blue'>blue</option>";
-            echo "<option value='purple'>purple</option>";
-            echo "<option value='grey'>grey</option>";
-            echo "<option value='brown'>brown</option>";
-            echo "<option value='black'>black</option>";
-            echo "<option value='teal'>teal</option>";
-            echo "<option value='red'>red</option>";
-            echo "<option value='orange'>orange</option>";
-            echo "<option value='yellow'>yellow</option>";
-            echo "<option value='green'>green</option>";
-
-            echo "</select>";
-            echo "</td>";
+            echo "<td>" . $select4 . "</td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
@@ -188,22 +177,7 @@
 
         if ($i == 5) {
             echo "<tr>";
-            echo "<td style='width: 20%;'>";
-            echo "<select name='color' id='select5'>";
-
-            echo "<option value='purple'>purple</option>";
-            echo "<option value='grey'>grey</option>";
-            echo "<option value='brown'>brown</option>";
-            echo "<option value='black'>black</option>";
-            echo "<option value='teal'>teal</option>";
-            echo "<option value='red'>red</option>";
-            echo "<option value='orange'>orange</option>";
-            echo "<option value='yellow'>yellow</option>";
-            echo "<option value='green'>green</option>";
-            echo "<option value='blue'>blue</option>";
-
-            echo "</select>";
-            echo "</td>";
+            echo "<td>" . $select5 . "</td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
@@ -211,22 +185,7 @@
 
         if ($i == 6) {
             echo "<tr>";
-            echo "<td style='width: 20%;'>";
-            echo "<select name='color' id='select6'>";
-
-            echo "<option value='grey'>grey</option>";
-            echo "<option value='brown'>brown</option>";
-            echo "<option value='black'>black</option>";
-            echo "<option value='teal'>teal</option>";
-            echo "<option value='red'>red</option>";
-            echo "<option value='orange'>orange</option>";
-            echo "<option value='yellow'>yellow</option>";
-            echo "<option value='green'>green</option>";
-            echo "<option value='blue'>blue</option>";
-            echo "<option value='purple'>purple</option>";
-
-            echo "</select>";
-            echo "</td>";
+            echo "<td>" . $select6 . "</td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
@@ -234,22 +193,7 @@
 
         if ($i == 7) {
             echo "<tr>";
-            echo "<td style='width: 20%;'>";
-            echo "<select name='color' id='select7'>";
-
-            echo "<option value='brown'>brown</option>";
-            echo "<option value='black'>black</option>";
-            echo "<option value='teal'>teal</option>";
-            echo "<option value='red'>red</option>";
-            echo "<option value='orange'>orange</option>";
-            echo "<option value='yellow'>yellow</option>";
-            echo "<option value='green'>green</option>";
-            echo "<option value='blue'>blue</option>";
-            echo "<option value='purple'>purple</option>";
-            echo "<option value='grey'>grey</option>";
-
-            echo "</select>";
-            echo "</td>";
+            echo "<td>" . $select7 . "</td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
@@ -257,22 +201,7 @@
 
         if ($i == 8) {
             echo "<tr>";
-            echo "<td style='width: 20%;'>";
-            echo "<select name='color' id='select8'>";
-
-            echo "<option value='black'>black</option>";
-            echo "<option value='teal'>teal</option>";
-            echo "<option value='red'>red</option>";
-            echo "<option value='orange'>orange</option>";
-            echo "<option value='yellow'>yellow</option>";
-            echo "<option value='green'>green</option>";
-            echo "<option value='blue'>blue</option>";
-            echo "<option value='purple'>purple</option>";
-            echo "<option value='grey'>grey</option>";
-            echo "<option value='brown'>brown</option>";
-
-            echo "</select>";
-            echo "</td>";
+            echo "<td>" . $select8 . "</td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
@@ -280,42 +209,13 @@
 
         if ($i == 9) {
             echo "<tr>";
-            echo "<td style='width: 20%;'>";
-            echo "<select name='color' id='select9'>";
-
-            echo "<option value='red'>red</option>";
-            echo "<option value='orange'>orange</option>";
-            echo "<option value='yellow'>yellow</option>";
-            echo "<option value='green'>green</option>";
-            echo "<option value='blue'>blue</option>";
-            echo "<option value='purple'>purple</option>";
-            echo "<option value='grey'>grey</option>";
-            echo "<option value='brown'>brown</option>";
-            echo "<option value='black'>black</option>";
-            echo "<option value='teal'>teal</option>";
-            ;
-
-            echo "</select>";
-            echo "</td>";
+            echo "<td>" . $select9 . "</td>";
             echo "<td style='width: 80%;'>";
             echo "</td>";
             echo "</tr>";
         }
     }
-    echo "</table>";
-
-    // echo "<table class='color-table'>";
-    // for ($i = 0; $i < $colors; $i++) {
-    //     echo "<tr>";
-    //     echo "<td style='width: 20%;'>" . $color_names[$i] . "</td>";
-    //     echo "<td style='width: 80%;'>";
-    //     echo "</td>";
-    //     echo "</tr>";
-    // }
-    // echo "</table>";
-    
-    // echo "<a href='printable_view.php?rows_cols=$rows_cols&colors=$colors' class='btn'>Printable View</a>";
-    
-    ?>
+    echo "</table>"
+        ?>
 
 </html>
